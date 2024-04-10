535f0c344e5b8c0fa79a9b7aa8e34ca00f785466
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-10 15:47:41 +0200

    feat: working one uncommented

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 let domElem = document.querySelector(".countdown");
 let seconds = 11;
 
-// const countdown = setInterval (function(){
-//     seconds--; 
-//     console.log(seconds);
-//     domElem.innerHTML = seconds;
+const countdown = setInterval (function(){
+    seconds--; 
+    console.log(seconds);
+    domElem.innerHTML = seconds;
 
 
-//     if (seconds < 0){
-//         clearInterval(countdown);
-//         console.log("AUGURI");
-//         domElem.innerHTML = "BUON ANNO";
-//     }
-// }, 1000) 
+    if (seconds < 0){
+        clearInterval(countdown);
+        console.log("AUGURI");
+        domElem.innerHTML = "BUON ANNO";
+    }
+}, 1000) 
 
-const countdown = setInterval (counter(seconds), 1000) 
+// const countdown = setInterval (counter(seconds), 1000) 
 
